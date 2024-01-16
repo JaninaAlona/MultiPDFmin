82cd0747247de5dc9c43431a7e2dae00ac5f7c87
jschro10 <janina_jessika_jelena.schroeder@smail.th-koeln.de>
2024-01-16 10:31:34 +0100

    input_control_script.js created and referenced in all modules, selected set correctly in HTML

--- docs/js/pdf_reader_script.js
+++ docs/js/pdf_reader_script.js
@@ -743,55 +743,6 @@ async function setPageRotation(pdfDoc, currentPage, newRotation) {
 }
 
 
-function restrictInputValues(inputId, min, max, parseIntOperation, parseFloatOperation) {
-    const inputElem = document.getElementById(inputId);
-    let valToRestrict;
-    inputElem.addEventListener('change', function() {
-        valToRestrict = inputElem.value;
-        valToRestrict = valToRestrict.replace(/\s+/g,'');
-        document.getElementById(inputId).value = valToRestrict;
-        if (valToRestrict.match(/^-?\d+$/) || valToRestrict.match(/^\d+\.\d+$/)) {
-            if (parseIntOperation) {
-                valToRestrict = parseInt(valToRestrict);
-                document.getElementById(inputId).value = valToRestrict;
-            } 
-            if (parseFloatOperation) {
-                valToRestrict = parseFloat(valToRestrict);
-            }
-            if (valToRestrict >= min && valToRestrict <= max) {
-                document.getElementById(inputId).value = valToRestrict;
-            } else {
-                if (valToRestrict < min) {
-                    document.getElementById(inputId).value = min;
-                } else if (valToRestrict > max) {
-                    document.getElementById(inputId).value = max;
-                }
-            }
-        }
-    }, false);
-}
-
-
-function convertInputToSucess(input, min, max, parseIntOperation, parseFloatOperation) {
-    let outputVal = input;
-
-    // valid positive/negative integer or valid float
-    if (outputVal.match(/^-?\d+$/) || outputVal.match(/^\d+\.\d+$/)) {
-        if (parseIntOperation) {
-            outputVal = parseInt(outputVal);
-        } 
-        if (parseFloatOperation) {
-            outputVal = parseFloat(outputVal);
-        }
-        if (!(outputVal >= min && outputVal <= max)) {
-            outputVal = -1000;
-        } 
-    } else {
-        outputVal = -1000;
-    }
-    return outputVal;
-}
-
 
 function resetUserModes() {
     if (userModes.length > 0) {
